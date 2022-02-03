52945e64917db800254c1908642af0db0ea06dee
Kristine Laing <kristal.tasha@gmail.com>
2022-02-03 02:06:07 +0000

    category nav added

---
+++ public/logic/category.js
@@ -0,0 +1,17 @@
+const cattype = document.querySelectorAll('.catitem');
+
+cattype.forEach(catt => {
+
+    const catte = catt.innerHTML;
+    const endpoint = `/category/${catte}`;
+
+    // console.log(catte);
+
+    catt.addEventListener("click", () => {
+        fetch(endpoint, {
+            method: "GET"
+        }).then(res => window.location.href = `/category/${catte}`).catch(err =>
+            console.log(err))
+
+    })
+})
